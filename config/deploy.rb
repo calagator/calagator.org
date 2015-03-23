bed9940b729ce378137e9a00780effcab1f9ef62
Reid Beels <mail@reidbeels.com>
2015-03-22 22:09:19 -0700

    Update repo URL for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :application, 'calagator'
-set :repo_url, 'git@github.com:calagator/calagator.git'
+set :repo_url, 'git@github.com:calagator/calagator.org.git'
 
 ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
