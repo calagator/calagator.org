ff56f02cf9a1b6d0d572b94352e2a3a3328a2c57
Reid Beels <mail@reidbeels.com>
2015-06-13 22:57:49 -0700

    Move puma worker killer config to puma.rb

--- config/initializers/puma_worker_killer.rb
+++ config/initializers/puma_worker_killer.rb
@@ -1,7 +1,9 @@
-PumaWorkerKiller.config do |config|
-  config.ram           = 1996 # mb
-  config.frequency     = 5    # seconds
-  config.percent_usage = 0.5
-end
+# now loaded from within puma.rb
 
-PumaWorkerKiller.start
+#PumaWorkerKiller.config do |config|
+  #config.ram           = 1996 # mb
+  #config.frequency     = 5    # seconds
+  #config.percent_usage = 0.5
+#end
+
+#PumaWorkerKiller.start
