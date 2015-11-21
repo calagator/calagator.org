f39dc5f4161f3c5f1f2ffa8d04b2679399a37862
Reid Beels <mail@reidbeels.com>
2015-11-21 16:28:38 +0100

    Use the encrypted key file for deployment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
 set :rails_env, 'production'
 server 'lucca.stumptownsyndicate.org', user: 'calagator', roles: %w{app db web}
+set :ssh_options, keys: ["config/deploy/deploy_rsa"] if File.exist?("config/deploy/deploy_rsa")
 
