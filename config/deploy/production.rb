9a918847f82b93699a39fb2bd49dc6006b07d523
Reid Beels <mail@reidbeels.com>
2015-11-21 16:32:36 +0100

    Set branch when deploying from Travis

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,7 @@
 set :rails_env, 'production'
 server 'lucca.stumptownsyndicate.org', user: 'calagator', roles: %w{app db web}
+
+# Travis Deployment
+set :branch, ENV['TRAVIS_BRANCH'] if ENV['TRAVIS_BRANCH']
 set :ssh_options, keys: ["config/deploy/deploy_rsa"] if File.exist?("config/deploy/deploy_rsa")
 
